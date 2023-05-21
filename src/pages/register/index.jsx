ae0bcc4d78351d7bd47f971ae134ea47fc5ee629
nguuyenthithuy <syn8749@gmail.com>
2023-05-21 17:19:57 +0700

    Axios and axios customize and sass

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,70 +1,78 @@
 import React from 'react';
 // import './index.css';
 import { Button, Checkbox, Form, Input } from 'antd';
+import './register.scss'
 
 const RegisterPage = () => {
     const onFinish = (values) => {
         console.log('Success:', values);
     };
     return (
-        <div className='register-page' style={{ padding: '50px' }}>
-            <h3 style={{ textAlign: 'center' }}>Đăng kí người dùng mới</h3>
-
-            <Form
-                name="basic"
-                labelCol={{ span: 5 }}
-                wrapperCol={{ span: 16 }}
-                style={{ maxWidth: 600, margin: '0 auto', marginTop: '50px' }}
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-                autoComplete="off"
-            >
-                <></>
-                <Form.Item
-                    label="Username"
-                    name="username"
-                    rules={[{ required: true, message: 'Please input your username!' }]}
-                >
-                    <Input />
-                </Form.Item>
-
-                <Form.Item
-                    label="Email"
-                    name="email"
-                    rules={[{ required: true, message: 'Please input your email!' }]}
-                >
-                    <Input />
-                </Form.Item>
-
-                <Form.Item
-                    label="Phone"
-                    name="phone"
-                    rules={[{ required: true, message: 'Please input your Phone!' }]}
-                >
-                    <Input />
-                </Form.Item>
 
 
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[{ required: true, message: 'Please input your password!' }]}
-                >
-                    <Input.Password />
-                </Form.Item>
 
-                <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
+        <div className='register-page'>
+            <div className='resgister-1' >
+                <div className='resgister-2'>
 
-                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                </Form.Item>
-            </Form>
+
+                    <h3 style={{ textAlign: 'center' }}>Đăng kí người dùng mới</h3>
+
+                    <Form
+                        name="basic"
+                        labelCol={{ span: 24 }}
+                        // wrapperCol={{ span: 16 }}
+                        // style={{ maxWidth: 600, margin: '0 auto', marginTop: '50px' }}
+                        // initialValues={{ remember: true }}
+                        onFinish={onFinish}
+                        autoComplete="off"
+                    >
+                        <></>
+                        <Form.Item
+                            labelCol={{ span: 24 }}
+                            label="Họ tên"
+                            name="username"
+                            rules={[{ required: true, message: 'Please input your username!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            labelCol={{ span: 24 }}
+                            label="Email"
+                            name="email"
+                            rules={[{ required: true, message: 'Please input your email!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            labelCol={{ span: 24 }}
+                            label="Số điện thoại"
+                            name="phone"
+                            rules={[{ required: true, message: 'Please input your Phone!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            labelCol={{ span: 24 }}
+                            label="Mật khẩu"
+                            name="password"
+                            rules={[{ required: true, message: 'Please input your password!' }]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+
+                        <Form.Item wrapperCol={{ span: 24 }}>
+                            <Button type="primary" htmlType="submit" loading={false}>
+                                Đăng kí
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
+            </div>
         </div>
-
 
     )
 }
