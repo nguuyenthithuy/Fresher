dc0a4d5be49447832bc2fb964896c2722e746f06
nguuyenthithuy <syn8749@gmail.com>
2023-05-22 19:42:41 +0700

    Bai tap chuc nang Register, goi api and xu li api and sass trang login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,73 @@
+
+import { Button, Checkbox, Form, Input, message, notification } from 'antd';
+import './login.scss';
+import { useNavigate } from 'react-router-dom';
+
+
+
 const LoginPage = () => {
+
+    const navigate = useNavigate();
+    const onFinish = (values) => {
+        console.log("check login")
+    }
+    const handleOnclick = () => {
+        navigate('/register')
+    }
+
+
     return (
         <>
-            Login page
+            <div className='register-page'>
+                <div className='resgister-1' >
+                    <div className='resgister-2'>
+
+
+                        <h3 style={{ textAlign: 'center' }}>Đăng nhập</h3>
+
+                        <Form
+                            name="basic"
+                            labelCol={{ span: 24 }}
+                            // wrapperCol={{ span: 16 }}
+                            // style={{ maxWidth: 600, margin: '0 auto', marginTop: '50px' }}
+                            // initialValues={{ remember: true }}
+                            onFinish={onFinish}
+                            autoComplete="off"
+                        >
+                            <></>
+                            <Form.Item
+                                labelCol={{ span: 24 }}
+                                label="Họ tên"
+                                name="fullName"
+                                rules={[{ required: true, message: 'Please input your username!' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+
+                            <Form.Item
+                                labelCol={{ span: 24 }}
+                                label="Mật khẩu"
+                                name="password"
+                                rules={[{ required: true, message: 'Please input your password!' }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+
+                            <Form.Item wrapperCol={{ span: 24 }}>
+                                <Button type="primary" htmlType="submit" loading={false}>
+                                    Đăng nhập
+                                </Button>
+                            </Form.Item>
+                        </Form>
+
+                        <h3 style={{ textAlign: 'center' }}>Or</h3>
+                        <div className='size'>
+                            <div >Chưa có tài khoản ? <span className='colo' onClick={() => handleOnclick()}> Đăng kí</span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
         </>
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
-// import './index.css';
-import { Button, Checkbox, Form, Input } from 'antd';
+import React, { useState } from 'react';
+
+import { Button, Checkbox, Form, Input, message, notification } from 'antd';
 import './register.scss'
+import { useNavigate } from 'react-router-dom';
+import { callRegister } from '../../services/api';
+
+
 
 const RegisterPage = () => {
-    const onFinish = (values) => {
-        console.log('Success:', values);
+
+    const navigate = useNavigate();
+    const [isSubmit, setIsSubmit] = useState(false)
+
+    const handleOnclick = () => {
+        navigate('/login')
+    }
+
+
+    const onFinish = async (values) => {
+        const { fullName, email, password, phone } = values;
+        setIsSubmit(true);
+        const res = await callRegister(fullName, email, password, phone);
+        setIsSubmit(false);
+        if (res?.data?._id) { // ?. nghĩa là thay chữ &&
+            message.success("Đăng kí thành công");
+            navigate('/login')
+        }
+        else {
+            notification.error({
+                message: "Có lỗi xảy ra",
+                description:
+                    res.message && res.message.length > 0 ? res.message[0] : res.message,
+                duration: 5,
+            })
+        }
+
+
+
+
     };
+
     return (
 
 
@@ -31,7 +64,7 @@ const RegisterPage = () => {
                         <Form.Item
                             labelCol={{ span: 24 }}
                             label="Họ tên"
-                            name="username"
+                            name="fullName"
                             rules={[{ required: true, message: 'Please input your username!' }]}
                         >
                             <Input />
@@ -65,10 +98,17 @@ const RegisterPage = () => {
                         </Form.Item>
 
                         <Form.Item wrapperCol={{ span: 24 }}>
-                            <Button type="primary" htmlType="submit" loading={false}>
+                            <Button type="primary" htmlType="submit" loading={isSubmit}>
                                 Đăng kí
                             </Button>
                         </Form.Item>
+
+                        <h3 style={{ textAlign: 'center' }}>Or</h3>
+                        <div className='size'>
+                            <div >Đã có tài khoản ? <span className='colo' onClick={() => handleOnclick()}> Đăng nhập</span></div>
+                        </div>
+
+
                     </Form>
                 </div>
             </div>
